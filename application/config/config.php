8b68efa7209a19c7ae53fa996a2c7718f56323c4
Rico-Macchi <robertmacchi44@gmail.com>
2020-11-30 08:54:36 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://wearemountains.com/shopify-gallery';
+$config['base_url'] = 'https://links-gallery-dev.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
